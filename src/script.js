130421350b4b67d6f5aaa5cc05ccf82e859688bc
Lucy Nowland <lucynowland03@gmail.com>
2023-12-08 11:40:58 +0000

    Added the functionality for the select cities

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 // Oxford
 function updateOxfordTime() {
   let oxfordElement = document.querySelector("#oxford");
-  let oxfordDateElement = oxfordElement.querySelector(".current-date");
-  let oxfordTimeElement = oxfordElement.querySelector(".current-time");
-  let oxfordTime = moment().tz("Europe/London");
+  if (oxfordElement) {
+    let oxfordDateElement = oxfordElement.querySelector(".current-date");
+    let oxfordTimeElement = oxfordElement.querySelector(".current-time");
+    let oxfordTime = moment().tz("Europe/London");
 
-  oxfordDateElement.innerHTML = oxfordTime.format("MMMM Do YYYY");
-  oxfordTimeElement.innerHTML = oxfordTime.format(
-    "hh:mm:ss [<small>]A[</small]"
-  );
+    oxfordDateElement.innerHTML = oxfordTime.format("MMMM Do YYYY");
+    oxfordTimeElement.innerHTML = oxfordTime.format(
+      "hh:mm:ss [<small>]A[</small]"
+    );
+  }
 }
 
 updateOxfordTime();
@@ -17,12 +19,17 @@ setInterval(updateOxfordTime, 1000);
 // Salem
 function updateSalemTime() {
   let salemElement = document.querySelector("#salem");
-  let salemDateElement = salemElement.querySelector(".current-date");
-  let salemTimeElement = salemElement.querySelector(".current-time");
-  let salemTime = moment().tz("America/Denver");
 
-  salemDateElement.innerHTML = salemTime.format("MMMM Do YYYY");
-  salemTimeElement.innerHTML = salemTime.format("hh:mm:ss [<small>]A[</small]");
+  if (salemElement) {
+    let salemDateElement = salemElement.querySelector(".current-date");
+    let salemTimeElement = salemElement.querySelector(".current-time");
+    let salemTime = moment().tz("America/Denver");
+
+    salemDateElement.innerHTML = salemTime.format("MMMM Do YYYY");
+    salemTimeElement.innerHTML = salemTime.format(
+      "hh:mm:ss [<small>]A[</small]"
+    );
+  }
 }
 
 updateSalemTime();
@@ -31,17 +38,43 @@ setInterval(updateSalemTime, 1000);
 // Aix-en-Provence
 function updateProvenceTime() {
   let aixEnProvenceElement = document.querySelector("#aix-en-provence");
-  let aixEnProvenceDateElement =
-    aixEnProvenceElement.querySelector(".current-date");
-  let aixEnProvenceTimeElement =
-    aixEnProvenceElement.querySelector(".current-time");
-  let aixEnProvenceTime = moment().tz("Europe/Paris");
+  if (aixEnProvenceElement) {
+    let aixEnProvenceDateElement =
+      aixEnProvenceElement.querySelector(".current-date");
+    let aixEnProvenceTimeElement =
+      aixEnProvenceElement.querySelector(".current-time");
+    let aixEnProvenceTime = moment().tz("Europe/Paris");
 
-  aixEnProvenceDateElement.innerHTML = aixEnProvenceTime.format("MMMM Do YYYY");
-  aixEnProvenceTimeElement.innerHTML = aixEnProvenceTime.format(
-    "hh:mm:ss [<small>]A[</small]"
-  );
+    aixEnProvenceDateElement.innerHTML =
+      aixEnProvenceTime.format("MMMM Do YYYY");
+    aixEnProvenceTimeElement.innerHTML = aixEnProvenceTime.format(
+      "hh:mm:ss [<small>]A[</small]"
+    );
+  }
 }
 
 updateProvenceTime();
 setInterval(updateProvenceTime, 1000);
+
+// Select Cities
+let selectCitiesElement = document.querySelector("#select-cities");
+
+function updateCity(event) {
+  let timeZone = event.target.value;
+  let cityTime = moment.tz(timeZone);
+  let cityName = timeZone.replace("Warsaw", "Krakow").split("/")[1];
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="current-date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+
+          <div class="current-time">${cityTime.format(
+            "hh:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
+selectCitiesElement.addEventListener("change", updateCity);
